34a6909c79a31a7ff9fae57e1d48952ca5fc5c48
montasirmoyen <montasirmoyen@gmail.com>
2026-01-23 17:11:42 -0500

    Update data

--- src/lib/types/universities.ts
+++ src/lib/types/universities.ts
@@ -6,17 +6,23 @@ export interface Building {
     "nearest-parking": string[];
 }
 
+export interface Entity {
+    images: string[];
+    address: string;
+    website?: string;
+}
+
 export interface University {
     name: string,
     location: string,
     viewImages?: Record<string, string[]>,
     // for now, the buildings have the same nearest-mbta and nearest-parking data,
     // this is intentional and for placeholder only, I'll change it later
-    buildings?: Record<string, Building>, 
-    residenceHallImages?: Record<string, string[]>,
-    parkingImages?: Record<string, string[]>,
-    mbtaStationImages?: Record<string, Record<string, string[]>>,
-    commonAreasImages?: Record<string, string[]>,
+    buildings?: Record<string, Building>,
+    residenceHalls?: Record<string, Entity>,
+    parkingGarages?: Record<string, Entity>,
+    mbtaStations?: Record<string, Record<string, Entity>>,
+    commonAreas?: Record<string, string[]>,
 };
 
 export const UNIVERSITIES: University[] = [
@@ -109,34 +115,92 @@ export const UNIVERSITIES: University[] = [
                 "nearest-parking": ["73-tremont", "center-plaza"],
             },
         },
-        residenceHallImages: {
-            "smith": [path + "su/smith-hall-1.jpg"],
-            "court": [path + "su/court-hall-1.jpg"],
-            "miller": [path + "su/miller-hall-1.jpg"],
-            "west": [path + "su/west-hall-1.jpg"],
+        residenceHalls: {
+            "smith": {
+                images: [path + "su/smith-hall-1.jpg"],
+                address: "150 Tremont Street, Boston, MA 02114",
+            },
+            "court": {
+                images: [path + "su/court-hall-1.jpg"],
+                address: "1 Court Street, Boston, MA 02114",
+            },
+            "miller": {
+                images: [path + "su/miller-hall-1.jpg"],
+                address: "6 Beacon Street #1120, Boston, MA 02114",
+            },
+            "west": {
+                images: [path + "su/west-hall-1.jpg"],
+                address: "10 West Street, Boston, MA 02111",
+            },
+            "modern-theatre": {
+                images: [path + "su/mt-hall-1.jpg"],
+                address: "525 Washington Steet, Boston, MA 02111",
+            },
         },
-        parkingImages: {
-            "73-tremont": [path + "su/73tremont-parking-1.jpg"],
-            "center-plaza": [path + "su/center-plaza-parking-1.jpg"],
-            "charles-river-garage": [path + "su/crg-parking-1.jpg"],
+        parkingGarages: {
+            "73-tremont": {
+                images: [path + "su/73tremont-parking-1.jpg"],
+                address: "73 Tremont Street, Boston, MA 02114",
+            },
+            "center-plaza": {
+                images: [path + "su/center-plaza-parking-1.png"],
+                address: "30 Somerset Street, Boston, MA 02114",
+            },
+            "charles-river-garage": {
+                images: [path + "su/crg-parking-1.jpg"],
+                address: "207 Cambridge Street, Boston, MA 02114",
+            },
         },
-        mbtaStationImages: {
+        mbtaStations: {
             "blue-line": {
-                "bowdoin": [path + "su/bowdoin-bl-mbta-1.jpg"],
-                "government-center": [path + "su/govtctr-bl-gl-mbta-1.jpg"],
-                "state": [path + "su/state-bl-ol-mbta-1.avif"],
+                "bowdoin": {
+                    images: [path + "su/bowdoin-bl-mbta-1.jpg"],
+                    address: "Bowdoin Square, Boston, MA 02114",
+                    website: "https://www.mbta.com/stops/place-bomnl",
+                },
+                "government-center": {
+                    images: [path + "su/govtctr-bl-gl-mbta-1.jpg"],
+                    address: "100 Hanover St, Boston, MA 02108",
+                    website: "https://www.mbta.com/stops/place-gover",
+                },
+                "state": {
+                    images: [path + "su/state-bl-ol-mbta-1.avif"],
+                    address: "State St, Boston, MA 02109",
+                    website: "https://www.mbta.com/stops/place-state",
+                },
             },
             "orange-line": {
-                "haymarket": [path + "su/haymarket-ol-mbta-1.jpg"],
-                "state": [path + "su/state-bl-ol-mbta-1.avif"],
+                "haymarket": {
+                    images: [path + "su/haymarket-ol-mbta-1.jpeg"],
+                    address: "200 Hanover St, Boston, MA 02113",
+                    website: "https://www.mbta.com/stops/place-haymt",
+                },
+                "state": {
+                    images: [path + "su/state-bl-ol-mbta-1.avif"],
+                    address: "State St, Boston, MA 02109",
+                    website: "https://www.mbta.com/stops/place-state",
+                },
             },
             "red-line": {
-                "park-street": [path + "su/parkst-rl-gl-mbta-1.jpg"],
+                "park-street": {
+                    images: [path + "su/parkst-rl-gl-mbta-1.jpg"],
+                    address: "100 Tremont St, Boston, MA 02108",
+                    website: "https://www.mbta.com/stops/place-pktrm",
+                },
             },
             "green-line": {
-                "government-center": [path + "su/govtctr-bl-gl-mbta-1.jpg"],
-                "park-street": [path + "su/parkst-rl-gl-mbta-1.jpg"],
+                "government-center": {
+                    images: [path + "su/govtctr-bl-gl-mbta-1.jpg"],
+                    address: "100 Hanover St, Boston, MA 02108",
+                    website: "https://www.mbta.com/stops/place-gover",
+                },
+                "park-street": {
+                    images: [path + "su/parkst-rl-gl-mbta-1.jpg"],
+                    address: "100 Tremont St, Boston, MA 02108",
+                    website: "https://www.mbta.com/stops/place-pktrm",
+                },
             }
         },
+
     },
 ];
