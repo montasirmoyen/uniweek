c13241c3e6e4ddd61bbd2bc808e4e7fabf32877a
montasirmoyen <montasirmoyen@gmail.com>
2026-03-12 01:53:19 -0400

    Refactor layout component to enhance structure and improve readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,24 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { Geist_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { Navbar1 } from "@/components/navbar1";
+import Footer from "@/components/footer";
 import { cn } from "@/lib/utils";
+import { AuthProvider } from "@/lib/contexts/AuthContext";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
+
+export const metadata = {
+  title: "UniWeek",
+  description: "Organize your class schedule",
+};
 
 export default function RootLayout({
   children,
@@ -23,7 +32,15 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <TooltipProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <AuthProvider>
+              <Navbar1 />
+              {children}
+              <Footer />
+            </AuthProvider>
+          </ThemeProvider>
+        </TooltipProvider>
       </body>
     </html>
   )
